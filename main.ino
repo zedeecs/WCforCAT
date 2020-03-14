bb08d8c12dd891046a8b9f0c4e6949b58812f997
zedeecs <669338500@qq.com>
2020-03-14 19:15:41 +0800

    merge

--- main.ino
+++ main.ino
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-=======
 /*
     程序主干
     作为整个控制系统的逻辑主干
 */
 
->>>>>>> 21da63f9962a6b17ea226349f5da037ad5c8cdf7
